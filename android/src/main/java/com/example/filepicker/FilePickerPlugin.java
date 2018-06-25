56c1c24ea3268bc37b8b1e7800f9094149475cb5
Miguel Ruivo <miguelpruivo@hotmail.com>
2018-06-25 17:44:13 +0100

    Fixes path invocation;

--- android/src/main/java/com/example/filepicker/FilePickerPlugin.java
+++ android/src/main/java/com/example/filepicker/FilePickerPlugin.java
@@ -1,10 +1,15 @@
 package com.example.filepicker;
 
+import android.Manifest;
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.util.Log;
 
+import in.gauriinfotech.commons.Commons;
 import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
@@ -15,8 +20,8 @@ import io.flutter.plugin.common.PluginRegistry.Registrar;
 /** FilePickerPlugin */
 public class FilePickerPlugin implements MethodCallHandler {
 
-
   private static final int REQUEST_CODE = 43;
+  private static final String permission = Manifest.permission.WRITE_EXTERNAL_STORAGE;
   private static Result result;
   private static Registrar instance;
 
@@ -24,24 +29,23 @@ public class FilePickerPlugin implements MethodCallHandler {
   public static void registerWith(Registrar registrar) {
     final MethodChannel channel = new MethodChannel(registrar.messenger(), "file_picker");
     channel.setMethodCallHandler(new FilePickerPlugin());
-    instance = registrar;
-    instance.addActivityResultListener(new PluginRegistry.ActivityResultListener()
-    {
-      @Override
-      public boolean onActivityResult(int requestCode, int resultCode, Intent data)
-      {
-        if(requestCode == REQUEST_CODE && resultCode == Activity.RESULT_OK) {
 
-          if(data != null) {
+    instance = registrar;
+    instance.addActivityResultListener(new PluginRegistry.ActivityResultListener() {
+      @Override
+      public boolean onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_CODE && resultCode == Activity.RESULT_OK) {
+
+          if (data != null) {
             Uri uri = data.getData();
-            result.success(uri.getPath());
+            String fullPath = Commons.getPath(uri, instance.context());
+            result.success(fullPath);
           }
 
         }
         return false;
       }
     });
-
 
   }
 
@@ -56,17 +60,29 @@ public class FilePickerPlugin implements MethodCallHandler {
     }
   }
 
-
-
-  private void startFileExplorer()
-  {
-
-    Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
-    intent.setType("application/pdf");
-    intent.addCategory(Intent.CATEGORY_OPENABLE);
-    instance.activity().startActivityForResult(intent,REQUEST_CODE);
-
+  private boolean checkPermission() {
+    Activity activity = instance.activity();
+    Log.i("SimplePermission", "Checking permission : " + permission);
+    return PackageManager.PERMISSION_GRANTED == ContextCompat.checkSelfPermission(activity, permission);
   }
 
+  private void requestPermission() {
+    Activity activity = instance.activity();
+    Log.i("File_Picker", "Requesting permission : " + permission);
+    String[] perm = {permission};
+    ActivityCompat.requestPermissions(activity, perm, 0);
+  }
+
+  private void startFileExplorer() {
+
+    if(checkPermission()) {
+      Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+      intent.setType("application/pdf");
+      intent.addCategory(Intent.CATEGORY_OPENABLE);
+      instance.activity().startActivityForResult(intent, REQUEST_CODE);
+    }else {
+      requestPermission();
+    }
+  }
 
 }
