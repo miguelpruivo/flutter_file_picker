3b6e3536ab0407b32b27bf26a1df587d20702a74
Miguel Ruivo <miguelpruivo@hotmail.com>
2018-12-06 15:15:57 +0000

    minor refactor

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   String _fileName = '...';
   String _path = '...';
-  FileType _pickingType = FileType.ANY;
+  FileType _pickingType;
 
   void _openFileExplorer() async {
     try {
