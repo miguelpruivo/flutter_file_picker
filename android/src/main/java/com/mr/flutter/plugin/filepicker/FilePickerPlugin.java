ec8afd4f57ddfd6b0248b7a45348f76b2c44169e
Vadym Pinchuk <vad.pinchuk@gmail.com>
2019-01-26 20:38:20 +0200

    Replace Support with AndroidX

--- android/src/main/java/com/mr/flutter/plugin/filepicker/FilePickerPlugin.java
+++ android/src/main/java/com/mr/flutter/plugin/filepicker/FilePickerPlugin.java
@@ -7,8 +7,8 @@ import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Environment;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import android.util.Log;
 import android.webkit.MimeTypeMap;
 
