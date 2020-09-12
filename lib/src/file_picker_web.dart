528241ad0037b46649fe6d394b49d3fb13d8c5c0
Alexandr <test@test.example>
2020-09-12 19:18:27 +0300

    Add support for mobile Safari

--- lib/src/file_picker_web.dart
+++ lib/src/file_picker_web.dart
@@ -10,11 +10,30 @@ import 'file_picker_result.dart';
 import 'platform_file.dart';
 
 class FilePickerWeb extends FilePicker {
-  FilePickerWeb._();
+  html.Element _target;
+  final String _kFilePickerInputsDomId = '__file_picker_web-file-input';
+
   static final FilePickerWeb platform = FilePickerWeb._();
+
+  FilePickerWeb._() {
+    _target = _ensureInitialized(_kFilePickerInputsDomId);
+  }
 
   static void registerWith(Registrar registrar) {
     FilePicker.platform = platform;
+  }
+
+  /// Initializes a DOM container where we can host input elements.
+  html.Element _ensureInitialized(String id) {
+    var target = html.querySelector('#${id}');
+    if (target == null) {
+      final html.Element targetElement =
+      html.Element.tag('flt-file-picker-inputs')..id = id;
+
+      html.querySelector('body').children.add(targetElement);
+      target = targetElement;
+    }
+    return target;
   }
 
   @override
@@ -34,36 +53,53 @@ class FilePickerWeb extends FilePicker {
     uploadInput.draggable = true;
     uploadInput.multiple = allowMultiple;
     uploadInput.accept = accept;
-    uploadInput.click();
 
-    uploadInput.onChange.listen((e) {
+    bool changeEventTriggered = false;
+    void changeEventListener (e) {
+      if (changeEventTriggered) {
+        return;
+      }
+      changeEventTriggered = true;
+
       final files = uploadInput.files;
-      final reader = html.FileReader();
 
       List<PlatformFile> pickedFiles = [];
 
-      reader.onLoadEnd.listen((e) {
-        final Uint8List bytes =
-            Base64Decoder().convert(reader.result.toString().split(",").last);
-
-        pickedFiles.add(
-          PlatformFile(
-            name: uploadInput.value.replaceAll('\\', '/'),
-            path: uploadInput.value,
-            size: bytes.length ~/ 1024,
-            bytes: withData ? bytes : null,
-          ),
-        );
-
-        if (pickedFiles.length >= files.length) {
-          filesCompleter.complete(pickedFiles);
-        }
-      });
-
       files.forEach((element) {
-        reader.readAsDataUrl(element);
+        final reader = html.FileReader();
+
+        reader.onLoadEnd.listen((e) {
+          Uint8List bytes;
+          if (withData) {
+            bytes = reader.result;
+          }
+
+          pickedFiles.add(
+            PlatformFile(
+              name: uploadInput.value.split('\\').last,
+              path: uploadInput.value,
+              size: bytes.length,
+              bytes: withData ? bytes : null,
+            ),
+          );
+
+          if (pickedFiles.length >= files.length) {
+            filesCompleter.complete(pickedFiles);
+          }
+        });
+
+        reader.readAsArrayBuffer(element);
       });
-    });
+    }
+
+    uploadInput.onChange.listen(changeEventListener);
+    uploadInput.addEventListener('change', changeEventListener);
+
+    //Add input element to the page body
+    _target.children.clear();
+    _target.children.add(uploadInput);
+    uploadInput.click();
+
     return FilePickerResult(await filesCompleter.future);
   }
 
