7f8b0330b67cfaface5b7a4a18d5ddfd45d63dd9
Vladimir Hudnitsky <xputnikx@gmail.com>
2023-07-13 15:19:37 +0200

    update dependencies

--- lib/src/windows/file_picker_windows.dart
+++ lib/src/windows/file_picker_windows.dart
@@ -9,7 +9,7 @@ import 'package:file_picker/src/utils.dart';
 import 'package:file_picker/src/exceptions.dart';
 import 'package:file_picker/src/windows/file_picker_windows_ffi_types.dart';
 import 'package:path/path.dart';
-import 'package:win32/winrt.dart';
+import 'package:win32/win32.dart';
 
 FilePicker filePickerWithFFI() => FilePickerWindows();
 
