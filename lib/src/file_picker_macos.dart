409b005623430b761c279f5499ad7ce8478de62f
Todd Volkert <tvolkert@Todds-MacBook-Pro.local>
2024-01-07 17:40:05 -0800

    Minor fixes
    * Allow m4v files in FileType.media filter
    * throw if caller tries to use allowedExtensiosn with any file
      type other than custom

--- lib/src/file_picker_macos.dart
+++ lib/src/file_picker_macos.dart
@@ -107,6 +107,10 @@ class FilePickerMacOS extends FilePicker {
   }
 
   String fileTypeToFileFilter(FileType type, List<String>? allowedExtensions) {
+    if (type != FileType.custom && (allowedExtensions?.isNotEmpty ?? false)) {
+      throw Exception(
+        'You are setting a type [$type]. Custom extension filters are only allowed with FileType.custom, please change it or remove filters.');
+    }
     switch (type) {
       case FileType.any:
         return '';
@@ -117,7 +121,7 @@ class FilePickerMacOS extends FilePicker {
       case FileType.image:
         return '"bmp", "gif", "jpeg", "jpg", "png"';
       case FileType.media:
-        return '"avi", "flv", "mkv", "mov", "mp4", "mpeg", "webm", "wmv", "bmp", "gif", "jpeg", "jpg", "png"';
+        return '"avi", "flv", "m4v", "mkv", "mov", "mp4", "mpeg", "webm", "wmv", "bmp", "gif", "jpeg", "jpg", "png"';
       case FileType.video:
         return '"avi", "flv", "mkv", "mov", "mp4", "mpeg", "webm", "wmv"';
       default:
