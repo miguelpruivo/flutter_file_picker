a19c53b42da49f61e8c64ddf09da8dcba9688426
Mikhail Novoseltsev <51940183+Sameri11@users.noreply.github.com>
2024-08-06 22:08:06 +0500

    Update lib/file_picker.dart
    Co-authored-by: Navaron Bracke <brackenavaron@gmail.com>

--- lib/file_picker.dart
+++ lib/file_picker.dart
@@ -6,6 +6,7 @@ export './src/file_picker_result.dart';
 export './src/file_picker_macos.dart';
 export './src/linux/file_picker_linux.dart';
 export './src/file_picker_io.dart';
-// Conditional export needed for web to successfully compile.
+// Conditional export needed for web to successfully compile,
+// as `dart:ffi` is not available on the web.
 export './src/windows/file_picker_windows_stub.dart'
     if (dart.library.ffi) './src/windows/file_picker_windows.dart';
