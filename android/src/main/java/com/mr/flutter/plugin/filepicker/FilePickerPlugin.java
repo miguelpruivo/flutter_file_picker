b64b2bd506d25fdf6f42845effb13623154957bb
Abdelhamid Ahmed <abdelhamid@quote-m.com>
2024-12-15 13:26:42 +0200

    fixing registrar bug

--- android/src/main/java/com/mr/flutter/plugin/filepicker/FilePickerPlugin.java
+++ android/src/main/java/com/mr/flutter/plugin/filepicker/FilePickerPlugin.java
@@ -253,21 +253,16 @@ public class FilePickerPlugin implements MethodChannel.MethodCallHandler, Flutte
             }
         });
         this.observer = new LifeCycleObserver(activity);
-
-        if (registrar != null) {
-            // V1 embedding setup for activity listeners.
-            application.registerActivityLifecycleCallbacks(this.observer);
-            registrar.addActivityResultListener(this.delegate);
-        } else {
-            // V2 embedding setup for activity listeners.
-            activityBinding.addActivityResultListener(this.delegate);
-            this.lifecycle = FlutterLifecycleAdapter.getActivityLifecycle(activityBinding);
-            this.lifecycle.addObserver(this.observer);
-        }
+        // V2 embedding setup for activity listeners.
+        activityBinding.addActivityResultListener(this.delegate);
+        activityBinding.addRequestPermissionsResultListener(this.delegate);
+        this.lifecycle = FlutterLifecycleAdapter.getActivityLifecycle(activityBinding);
+        this.lifecycle.addObserver(this.observer);
     }
 
     private void tearDown() {
         this.activityBinding.removeActivityResultListener(this.delegate);
+        this.activityBinding.removeRequestPermissionsResultListener(this.delegate);
         this.activityBinding = null;
         if(this.observer != null) {
             this.lifecycle.removeObserver(this.observer);
