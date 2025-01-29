90c4513a55e4eb4f60f65cb46a27ae73d06d0573
Dominik Roszkowski <dominik@roszkowski.dev>
2025-01-29 13:29:01 +0100

    Remove XCTest

--- example/macos/RunnerTests/RunnerTests.swift
+++ example/macos/RunnerTests/RunnerTests.swift
@@ -2,7 +2,6 @@ import Cocoa
 import FlutterMacOS
 import XCTest
 
-
 @testable import file_picker
 
 // This demonstrates a simple unit test of the Swift portion of this plugin's implementation.
@@ -10,19 +9,4 @@ import XCTest
 // See https://developer.apple.com/documentation/xctest for more information about using XCTest.
 
 class RunnerTests: XCTestCase {
-
-  func testGetPlatformVersion() {
-    let plugin = FilePickerPlugin()
-
-    let call = FlutterMethodCall(methodName: "getPlatformVersion", arguments: [])
-
-    let resultExpectation = expectation(description: "result block must be called.")
-    plugin.handle(call) { result in
-      XCTAssertEqual(result as! String,
-                     "macOS " + ProcessInfo.processInfo.operatingSystemVersionString)
-      resultExpectation.fulfill()
-    }
-    waitForExpectations(timeout: 1)
-  }
-
 }
