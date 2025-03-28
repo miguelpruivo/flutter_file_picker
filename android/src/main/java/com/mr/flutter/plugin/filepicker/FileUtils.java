a76f735bbf53e74477f1392c27a98aead5467ccd
Victor Carreras <34163765+vicajilau@users.noreply.github.com>
2025-03-28 11:28:00 +0100

    restored SimpleDateFormat

--- android/src/main/java/com/mr/flutter/plugin/filepicker/FileUtils.java
+++ android/src/main/java/com/mr/flutter/plugin/filepicker/FileUtils.java
@@ -114,7 +114,7 @@ public class FileUtils {
     }
 
     private static File createImageFile(Context context) throws IOException {
-        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss", Locale.getDefault()).format(new Date());
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
         String imageFileName = "JPEG_" + timeStamp + "_";
         File storageDir = context.getCacheDir();
         return File.createTempFile(imageFileName, ".jpg", storageDir);
