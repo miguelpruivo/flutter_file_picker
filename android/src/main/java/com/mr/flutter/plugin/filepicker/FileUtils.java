f7202d5ffb8b5b4545f1999606eeff862b411d1a
Victor Carreras <34163765+vicajilau@users.noreply.github.com>
2025-03-28 11:34:54 +0100

    fixed import locale

--- android/src/main/java/com/mr/flutter/plugin/filepicker/FileUtils.java
+++ android/src/main/java/com/mr/flutter/plugin/filepicker/FileUtils.java
@@ -34,6 +34,7 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.Locale;
 import java.util.Random;
 
 public class FileUtils {
@@ -114,7 +115,7 @@ public class FileUtils {
     }
 
     private static File createImageFile(Context context) throws IOException {
-        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss", Locale.getDefault()).format(new Date());
         String imageFileName = "JPEG_" + timeStamp + "_";
         File storageDir = context.getCacheDir();
         return File.createTempFile(imageFileName, ".jpg", storageDir);
