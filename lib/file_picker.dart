549d8399ca58df44ecfe856ab376394e4d57ff37
Victor Carreras <34163765+vicajilau@users.noreply.github.com>
2025-12-27 16:45:16 +0100

    feat: Hide desktop platform exports on web builds.

--- lib/file_picker.dart
+++ lib/file_picker.dart
@@ -2,6 +2,9 @@ export 'src/file_picker.dart';
 export 'src/api/platform_file.dart';
 export 'src/api/file_picker_result.dart';
 export 'src/api/file_picker_types.dart';
-export 'src/platform/linux/file_picker_linux.dart';
-export 'src/platform/macos/file_picker_macos.dart';
-export 'src/platform/windows/file_picker_windows.dart';
+export 'src/platform/linux/file_picker_linux.dart'
+    if (dart.library.html) 'src/web_hysteresis.dart';
+export 'src/platform/macos/file_picker_macos.dart'
+    if (dart.library.html) 'src/web_hysteresis.dart';
+export 'src/platform/windows/file_picker_windows.dart'
+    if (dart.library.html) 'src/web_hysteresis.dart';
