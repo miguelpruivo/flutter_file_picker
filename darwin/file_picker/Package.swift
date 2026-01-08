d9fcc365fb7687dbf3cba3f480bda70cf5f33067
Victor Carreras <34163765+vicajilau@users.noreply.github.com>
2026-01-08 19:49:34 +0100

    refactor(Package.swift): Remove TODO comments and streamline target configuration

--- darwin/file_picker/Package.swift
+++ darwin/file_picker/Package.swift
@@ -10,27 +10,15 @@ let package = Package(
         .macOS("10.13")
     ],
     products: [
-        // TODO: Update your library and target names.
-        // If the plugin name contains "_", replace with "-" for the library name.
         .library(name: "file-picker", targets: ["file_picker"])
     ],
     dependencies: [],
     targets: [
         .target(
-            // TODO: Update your target name.
             name: "file_picker",
             dependencies: [],
             resources: [
-                // TODO: If your plugin requires a privacy manifest
-                // (e.g. if it uses any required reason APIs), update the PrivacyInfo.xcprivacy file
-                // to describe your plugin's privacy impact, and then uncomment this line.
-                // For more information, see:
-                // https://developer.apple.com/documentation/bundleresources/privacy_manifest_files
-                // .process("PrivacyInfo.xcprivacy"),
-
-                // TODO: If you have other resources that need to be bundled with your plugin, refer to
-                // the following instructions to add them:
-                // https://developer.apple.com/documentation/xcode/bundling-resources-with-a-swift-package
+                .process("PrivacyInfo.xcprivacy"),
             ]
         )
     ]
