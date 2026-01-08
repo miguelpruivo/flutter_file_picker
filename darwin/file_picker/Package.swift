7b3e75d25f80ab2643f8ee073cb171c1e2fe2b45
Victor Carreras <34163765+vicajilau@users.noreply.github.com>
2026-01-08 16:33:06 +0100

    Refactor file_picker plugin by removing iOS-specific files and consolidating shared code for macOS and iOS. Deleted unused utility classes and updated podspec and package files to reflect changes in structure. Enhanced pubspec.yaml to enable sharedDarwinSource for iOS and macOS platforms.

---
+++ darwin/file_picker/Package.swift
@@ -0,0 +1,37 @@
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "file_picker",
+    platforms: [
+        .iOS("14.0"),
+        .macOS("10.13")
+    ],
+    products: [
+        // TODO: Update your library and target names.
+        // If the plugin name contains "_", replace with "-" for the library name.
+        .library(name: "file-picker", targets: ["file_picker"])
+    ],
+    dependencies: [],
+    targets: [
+        .target(
+            // TODO: Update your target name.
+            name: "file_picker",
+            dependencies: [],
+            resources: [
+                // TODO: If your plugin requires a privacy manifest
+                // (e.g. if it uses any required reason APIs), update the PrivacyInfo.xcprivacy file
+                // to describe your plugin's privacy impact, and then uncomment this line.
+                // For more information, see:
+                // https://developer.apple.com/documentation/bundleresources/privacy_manifest_files
+                // .process("PrivacyInfo.xcprivacy"),
+
+                // TODO: If you have other resources that need to be bundled with your plugin, refer to
+                // the following instructions to add them:
+                // https://developer.apple.com/documentation/xcode/bundling-resources-with-a-swift-package
+            ]
+        )
+    ]
+)
