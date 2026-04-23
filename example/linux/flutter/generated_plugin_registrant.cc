374263218d92ad323c94b91f6cb528ae1e395768
yangzhao <yangzhaojava@gmail.com>
2026-04-23 22:23:39 +0800

    merge master

--- example/linux/flutter/generated_plugin_registrant.cc
+++ example/linux/flutter/generated_plugin_registrant.cc
@@ -6,6 +6,10 @@
 
 #include "generated_plugin_registrant.h"
 
+#include <file_picker/file_picker_plugin.h>
 
 void fl_register_plugins(FlPluginRegistry* registry) {
+  g_autoptr(FlPluginRegistrar) file_picker_registrar =
+      fl_plugin_registry_get_registrar_for_plugin(registry, "FilePickerPlugin");
+  file_picker_plugin_register_with_registrar(file_picker_registrar);
 }
--- example/linux/flutter/generated_plugins.cmake
+++ example/linux/flutter/generated_plugins.cmake
@@ -3,6 +3,7 @@
 #
 
 list(APPEND FLUTTER_PLUGIN_LIST
+  file_picker
 )
 
 list(APPEND FLUTTER_FFI_PLUGIN_LIST
