54fb5f76101f224fc99d37116b6ae8ba42aa71b1
djimene2 <122808735+daniJimen@users.noreply.github.com>
2026-06-10 14:47:54 +0200

    feat: clean up .gitignore by removing duplicate entry for GeneratedPluginRegistrant

--- file_picker/lib/file_picker.dart
+++ file_picker/lib/file_picker.dart
@@ -3,5 +3,5 @@ export 'src/file_picker.dart';
 export 'src/file_picker_linux.dart' show FilePickerLinux;
 export 'src/platform/macos/file_picker_macos.dart' show FilePickerMacOS;
 export 'src/file_picker_windows_stub.dart'
-  if (dart.library.ffi) 'src/file_picker_windows.dart'
-  show FilePickerWindows;
+    if (dart.library.ffi) 'src/file_picker_windows.dart'
+    show FilePickerWindows;
