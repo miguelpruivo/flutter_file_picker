9fc0398b1e98bf359bec7b0d662fefd10e17df12
Victor Carreras <34163765+vicajilau@users.noreply.github.com>
2026-07-23 15:13:08 +0200

    Update packages/file_picker_platform_interface/lib/src/api/exceptions.dart
    Co-authored-by: Navaron Bracke <brackenavaron@gmail.com>

--- packages/file_picker_platform_interface/lib/src/api/exceptions.dart
+++ packages/file_picker_platform_interface/lib/src/api/exceptions.dart
@@ -1,4 +1,11 @@
 class IllegalCharacterInFileNameException implements Exception {
+  const IllegalCharacterInFileNameException(this.message);
+  
+  final String message;
+  
+  @override
+  String toString() => 'IllegalCharacterInFileNameException: $message';
+}
   final String message;
   IllegalCharacterInFileNameException(this.message);
   @override
