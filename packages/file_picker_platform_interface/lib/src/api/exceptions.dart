ca88402e3f76d09da196950d56e8ed1578eddf48
Victor Carreras <34163765+vicajilau@users.noreply.github.com>
2026-07-23 15:14:23 +0200

    refactor: improve documentation and formatting for IllegalCharacterInFileNameException

--- packages/file_picker_platform_interface/lib/src/api/exceptions.dart
+++ packages/file_picker_platform_interface/lib/src/api/exceptions.dart
@@ -1,13 +1,12 @@
+/// An exception thrown when a file name contains characters that are not allowed
+/// for the underlying platform.
 class IllegalCharacterInFileNameException implements Exception {
+  /// Creates an instance of [IllegalCharacterInFileNameException].
   const IllegalCharacterInFileNameException(this.message);
-  
+
+  /// The error message.
   final String message;
-  
-  @override
-  String toString() => 'IllegalCharacterInFileNameException: $message';
-}
-  final String message;
-  IllegalCharacterInFileNameException(this.message);
+
   @override
   String toString() => 'IllegalCharacterInFileNameException: $message';
 }
