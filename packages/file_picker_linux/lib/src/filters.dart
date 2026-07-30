c76282971c1fa8e9aaef29886c3f6f436aadf09d
Victor Carreras <34163765+vicajilau@users.noreply.github.com>
2026-07-30 06:30:43 +0200

    refactor(linux): address maintainer comments (static const fields, collection for, private extension lists)

--- packages/file_picker_linux/lib/src/filters.dart
+++ packages/file_picker_linux/lib/src/filters.dart
@@ -4,7 +4,54 @@ import 'package:file_picker_platform_interface/file_picker_platform_interface.da
 typedef FilterInfo = Map<String, List<(int, String)>>;
 
 class Filter {
+  static const List<String> _audioExtensions = [
+    "*.aac",
+    "*.midi",
+    "*.mp3",
+    "*.ogg",
+    "*.wav",
+  ];
+
+  static const List<String> _imageExtensions = [
+    "*.bmp",
+    "*.gif",
+    "*.jpeg",
+    "*.jpg",
+    "*.png",
+    "*.webp",
+  ];
+
+  static const List<String> _mediaExtensions = [
+    "*.avi",
+    "*.flv",
+    "*.m4v",
+    "*.mkv",
+    "*.mov",
+    "*.mp4",
+    "*.mpeg",
+    "*.webm",
+    "*.wmv",
+    "*.bmp",
+    "*.gif",
+    "*.jpeg",
+    "*.jpg",
+    "*.png",
+  ];
+
+  static const List<String> _videoExtensions = [
+    "*.avi",
+    "*.flv",
+    "*.mkv",
+    "*.mov",
+    "*.mp4",
+    "*.m4v",
+    "*.mpeg",
+    "*.webm",
+    "*.wmv",
+  ];
+
   FilterInfo info = {};
+
   Filter(FileType type, List<String>? allowedExtensions) {
     if (type == FileType.custom &&
         (allowedExtensions == null || allowedExtensions.isEmpty)) {
@@ -16,67 +63,18 @@ class Filter {
       case FileType.any:
         return;
       case FileType.audio:
-        final audio = ["*.aac", "*.midi", "*.mp3", "*.ogg", "*.wav"];
-        List<(int, String)> audioList = [];
-        for (var filter in audio) {
-          audioList.add((0, filter));
-        }
-        info["Audio"] = audioList;
+        info["Audio"] = [for (final ext in _audioExtensions) (0, ext)];
       case FileType.custom:
-        final custom = [...?allowedExtensions];
-        List<(int, String)> customList = [];
-        for (var filter in custom) {
-          customList.add((0, "*.$filter"));
-        }
-        info["Custom"] = customList;
+        info["Custom"] = [for (final ext in allowedExtensions!) (0, "*.$ext")];
       case FileType.image:
-        final image = ["*.bmp", "*.gif", "*.jpeg", "*.jpg", "*.png", "*.webp"];
-        List<(int, String)> imageList = [];
-        for (var filter in image) {
-          imageList.add((0, filter));
-        }
-        info["Image"] = imageList;
+        info["Image"] = [for (final ext in _imageExtensions) (0, ext)];
       case FileType.media:
-        final media = [
-          "*.avi",
-          "*.flv",
-          "*.m4v",
-          "*.mkv",
-          "*.mov",
-          "*.mp4",
-          "*.mpeg",
-          "*.webm",
-          "*.wmv",
-          "*.bmp",
-          "*.gif",
-          "*.jpeg",
-          "*.jpg",
-          "*.png",
-        ];
-        List<(int, String)> mediaList = [];
-        for (var filter in media) {
-          mediaList.add((0, filter));
-        }
-        info["Media"] = mediaList;
+        info["Media"] = [for (final ext in _mediaExtensions) (0, ext)];
       case FileType.video:
-        final video = [
-          "*.avi",
-          "*.flv",
-          "*.mkv",
-          "*.mov",
-          "*.mp4",
-          "*.m4v",
-          "*.mpeg",
-          "*.webm",
-          "*.wmv",
-        ];
-        List<(int, String)> videoList = [];
-        for (var filter in video) {
-          videoList.add((0, filter));
-        }
-        info["Video"] = videoList;
+        info["Video"] = [for (final ext in _videoExtensions) (0, ext)];
     }
   }
+
   DBusArray toDBusArray() {
     List<DBusValue> dataList = [];
     info.forEach((var key, var values) {
