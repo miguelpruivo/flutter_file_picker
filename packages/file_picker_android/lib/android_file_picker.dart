f7fc825d652d258f140c15c6af71f334593d49ed
Victor Carreras <34163765+vicajilau@users.noreply.github.com>
2026-08-12 10:12:43 +0200

    refactor: move file picker exports to new android_file_picker library file

---
+++ packages/file_picker_android/lib/android_file_picker.dart
@@ -0,0 +1,4 @@
+export 'src/android_platform_file.dart';
+export 'src/android_saf_handle.dart';
+export 'src/file_picker_android.dart';
+export 'src/file_picker_android_options.dart';
--- packages/file_picker_android/lib/file_picker_android.dart
+++ packages/file_picker_android/lib/file_picker_android.dart
@@ -1,4 +1,1 @@
-export 'src/android_platform_file.dart';
-export 'src/android_saf_handle.dart';
-export 'src/file_picker_android.dart';
-export 'src/file_picker_android_options.dart';
+export 'android_file_picker.dart';
